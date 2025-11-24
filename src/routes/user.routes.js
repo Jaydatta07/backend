4b868c1d90758cdabe5e09a0052bb2634391e59e
jaydatta shinde <shindejaydatta1312@gmail.com>
2025-11-25 00:54:24 +0530

    add registerUser controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,21 @@
 import express from "express"
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js"
 
 const router = Router()
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([//this is used to upload files and fields keyword accepts multiple files for for multiple inputs in array 
+        {
+            name: "avatar",//we will upload image for avatar in quantity of 1
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser)
 
 export default router
